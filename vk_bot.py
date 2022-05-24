7bfad65925742a236798b68bb97e7bbb1d63529f
MSMikl <michalbl4@gmail.com>
2022-05-24 19:47:26 +0300

    echobot for VK.

--- vk_bot.py
+++ vk_bot.py
@@ -1,7 +1,9 @@
 import logging
 import os
 
-import vk_api
+from random import randint
+
+import vk_api as vk
 
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -10,34 +12,31 @@ from dialog_funcs import detect_intent_text
 
 logger = logging.getLogger('tbot')
 
-load_dotenv()
-vk_token = os.getenv('VK_TOKEN')
-phone_number = os.getenv('PHONE_NUMBER')
-vk_password = os.getenv('VK_PASSWORD')
-project_id = os.getenv('GOOGLE_PROJECT_ID')
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 
-vk_session = vk_api.VkApi(token=vk_token)
-longpoll = VkLongPoll(vk_session)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=randint(1, 1000)
+    )
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-            print(
-                'От меня для: ',
-                event.user_id,
-                '\n {}'.format(detect_intent_text(
-                    project_id,
-                    event.user_id,
-                    event.text,
-                    'ru-RU'
-                ))
-            )
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+
+def main():
+    load_dotenv()
+    vk_token = os.getenv('VK_TOKEN')
+    project_id = os.getenv('GOOGLE_PROJECT_ID')
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    vk_session = vk.VkApi(token=vk_token)
+    longpoll = VkLongPoll(vk_session)
+    vk_api = vk_session.get_api()
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+
+
+if __name__ == '__main__':
+    main()
